edf3a4478b1ae657609598899393ca3c38486d87
Jake Betz <necropyre@outlook.com>
2017-10-17 16:08:29 -0700

    Renamed tunesControlLib to tunesCtrlLib. Added menu controls to frmMain.

--- tunesControl/frmMain.Designer.cs
+++ tunesControl/frmMain.Designer.cs
@@ -28,7 +28,62 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.tsFile = new System.Windows.Forms.ToolStripMenuItem();
+            this.tsOptions = new System.Windows.Forms.ToolStripMenuItem();
+            this.hELPToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.tsSource = new System.Windows.Forms.ToolStripMenuItem();
+            this.tsCheckForUpdate = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tsFile,
+            this.tsOptions,
+            this.hELPToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(658, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // tsFile
+            // 
+            this.tsFile.Name = "tsFile";
+            this.tsFile.Size = new System.Drawing.Size(40, 20);
+            this.tsFile.Text = "FILE";
+            // 
+            // tsOptions
+            // 
+            this.tsOptions.Name = "tsOptions";
+            this.tsOptions.Size = new System.Drawing.Size(69, 20);
+            this.tsOptions.Text = "OPTIONS";
+            this.tsOptions.Click += new System.EventHandler(this.tsOptions_Click);
+            // 
+            // hELPToolStripMenuItem
+            // 
+            this.hELPToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tsSource,
+            this.tsCheckForUpdate});
+            this.hELPToolStripMenuItem.Name = "hELPToolStripMenuItem";
+            this.hELPToolStripMenuItem.Size = new System.Drawing.Size(47, 20);
+            this.hELPToolStripMenuItem.Text = "HELP";
+            // 
+            // tsSource
+            // 
+            this.tsSource.Name = "tsSource";
+            this.tsSource.Size = new System.Drawing.Size(170, 22);
+            this.tsSource.Text = "Source";
+            this.tsSource.Click += new System.EventHandler(this.tsSource_Click);
+            // 
+            // tsCheckForUpdate
+            // 
+            this.tsCheckForUpdate.Name = "tsCheckForUpdate";
+            this.tsCheckForUpdate.Size = new System.Drawing.Size(170, 22);
+            this.tsCheckForUpdate.Text = "Check for updates";
+            this.tsCheckForUpdate.Click += new System.EventHandler(this.tsCheckForUpdates_Click);
             // 
             // frmMain
             // 
@@ -36,15 +91,28 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ActiveBorder;
             this.ClientSize = new System.Drawing.Size(658, 476);
+            this.Controls.Add(this.menuStrip1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.MainMenuStrip = this.menuStrip1;
             this.MaximizeBox = false;
             this.Name = "frmMain";
             this.Text = "Tunes Control";
+            this.Load += new System.EventHandler(this.frmMain_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem tsFile;
+        private System.Windows.Forms.ToolStripMenuItem tsOptions;
+        private System.Windows.Forms.ToolStripMenuItem hELPToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem tsSource;
+        private System.Windows.Forms.ToolStripMenuItem tsCheckForUpdate;
     }
 }
 
--- tunesControl/frmMain.cs
+++ tunesControl/frmMain.cs
@@ -16,5 +16,30 @@ namespace tunesControl
         {
             InitializeComponent();
         }
+
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+            //TODO
+            //bring up login first (asks for perms for the account)
+            //then pass the login API key and process the info using tunesControlLib
+
+        }
+
+        private void tsSource_Click(object sender, EventArgs e)
+        {
+            //TODO
+            //Open web browser that links to the project source
+        }
+
+        private void tsCheckForUpdates_Click(object sender, EventArgs e)
+        {
+            //TODO
+            //run a check for updates, use an AWS for version checking, then download the binary if nescessary
+        }
+
+        private void tsOptions_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
